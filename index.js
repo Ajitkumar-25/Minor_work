c2d49002178bac7b2c38d2d209073435dc7afd9b
Ajitkumar-25 <kajit0408@gmail.com>
2023-12-02 19:26:49 +0530

    added complete code

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.static("public"));
 const questions = [
     "Why we need to do algorithm analysis?",
     "What are the criteria of algorithm analysis?",
-    "What is asymptotic analysis of an algorithm?",
-    "What are asymptotic notations?",
     "What is linear data structure?",
     "What are common operations that can be performed on a data-structure?",
     "Briefly explain the approaches to develop algorithms.",
@@ -44,8 +42,6 @@ const questions = [
   const answers = [
     "A problem can be solved in more than one ways. So, many solution algorithms can be derived for a given problem. We analyze available algorithms to find and implement the best suitable algorithm.",
     "An algorithm is generally analyzed on two factors time and space. That is, how much execution time and how much extra space required by the algorithm.",
-    "Asymptotic analysis of an algorithm refers to defining the mathematical boundation/framing of its run time performance. Using asymptotic analysis, we can very well conclude the best case, average case, and worst-case scenario of an algorithm.",
-    "Asymptotic analysis can provide three levels of mathematical binding of execution time of an algorithm  Best case is represented by Ω(n) notation. Worst case is represented by Ο(n) notation. Average case is represented by Θ(n) notation.",
     "A linear data structure has sequentially arranged data items. The next time can be located in the next memory address. It is stored and accessed in a sequential manner. Array and list are examples of linear data structures.",
     "The following operations are commonly performed on any data-structure: Insertion adding a data item; Deletion  removing a data item; Traversal accessing and/or printing all data items; Searching finding a particular data item; Sorting arranging data items in a pre-defined sequence.",
     "There are three commonly used approaches to develop algorithms  Greedy Approach  finding a solution by choosing the next best option; Divide and Conquer  dividing the problem to a minimum possible sub-problem and solving them independently; Dynamic Programming dividing the problem to a minimum possible sub-problem and solving them combinedly.",
@@ -73,8 +69,8 @@ const questions = [
     "NP is the class of decision problems for which a given proposed solution for a given input can be checked quickly to see if it is really a solution.",
     "A simple graph is a graph that has not more than one edge between a pair of nodes than such a graph is called a simple graph.",
   ];
-
-
+  
+  
 app.use(express.json());
 let rec;
 app.get("/", function(req, res){
